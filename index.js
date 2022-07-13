d3dd76441205d4d14b884fa3cbfa6caee3982b95
lehuynhtuan <meowmeowlht@gmail.com>
2022-07-13 23:50:35 +0700

    bai3

--- index.js
+++ index.js
@@ -199,3 +199,84 @@ var odd = 0 ;
  if(a*a == b*b + c*c || b*b == a*a + c*a || c*c == a*a + b*b){
     console.log("Đây là tam giác vuông");
  }
+
+
+ var month = 1;
+
+/**
+ * if month === 1{
+ *      console.log("31 ngay")
+ * } else if(month === 2) {
+ *      console.log("28 ngày")     
+ * }
+ * 
+ * 
+ * 
+ */
+
+switch(month) {
+    case 1:
+        console.log("31 ngay");
+        break;
+    case 2:
+        console.log("28 ngay");
+        break;
+    default :
+        console.log("Không hợp lệ");    
+}
+
+
+// bài 3:
+
+/***
+ * INPUT: 3 số nguyên, in ra cách đọc nó
+ * 
+ * PROCESS:
+ * 1. lấy input
+ * 2. tính
+ * 3. log ra màn hình
+ * OUTPIT: cách đọc của 3 số nguyên
+ */
+
+var a = 2 % 10;
+var b = (30 / 10) % 10;
+var c = 200 / 100;
+
+if (a,b,c < 100 || a,b,c > 500) {
+    console.log("cách đọc của 3 số nguyên là:");
+}else{
+    switch(c){
+        case 1: console.log("Một trăm ");
+        break;
+        case 2: console.log("Hai trăm ");
+        break;
+        case 3: console.log("Ba trăm ");
+        break;
+        case 4: console.log("Bốn trăm ");
+        break;
+        case 5: console.log("Năm trăm ");
+        break;
+    }
+}
+if(b % 10 == 0 && a != 0){
+    console.log("lẻ ",b);
+  }else{
+  switch(b){
+    case 1: console.log("mười ");
+    break;
+    case 2: console.log("hai mươi ");
+    break;
+    case 3: console.log("ba mươi ");
+    break;
+    case 4: console.log("bốn mươi ");
+    break;
+  }
+}
+  switch(a){
+    case 1: console.log("một ");
+    break;
+    case 2: console.log("hai ");
+    break;
+    case 3: console.log("ba ");
+    break;
+  }
